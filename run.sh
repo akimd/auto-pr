8bad00f43fb94cc5e0608e46b660349732509dfe
Akim Demaille <akim.demaille@gmail.com>
2018-02-22 16:14:24 +0100

    Minor changes
    - consistent use of $BUILD_JSON
    - avoid multiple copies
    - be ready to work for old versions that don't have a editions.moby dir
    
    Signed-off-by: Akim Demaille <akim.demaille@gmail.com>

--- run.sh
+++ run.sh
@@ -6,28 +6,30 @@ git config --global user.email "${USER_EMAIL}"
 
 echo "Retrieve docker commit"
 getme Extract "${BUILD_URL}/bundles-ce-binary.tar.gz" "bundles/*/binary-client/docker" /tmp/docker
-set +e
-DOCKER_COMMIT=$(/tmp/docker version -f '{{ .Client.GitCommit }}')
-DOCKER_VERSION=$(/tmp/docker version -f '{{ .Client.Version }}')
-set -e
+
+DOCKER_COMMIT=$(/tmp/docker version -f '{{ .Client.GitCommit }}' || true)
+DOCKER_VERSION=$(/tmp/docker version -f '{{ .Client.Version }}' || true)
 echo "Docker commit: ${DOCKER_COMMIT}"
 
 BRANCH="docker-ce-${DOCKER_VERSION}-${DOCKER_COMMIT}"
 
 echo "Get sources"
 export GOPATH="/go"
+
 mkdir -p "$GOPATH/src/github.com/docker"
 cd "$GOPATH/src/github.com/docker"
 git clone -b ${BASE} git@github.com:${GITHUB_REPO}.git
+
 cd pinata
 git checkout -b "${BRANCH}"
 
-cp build.json build.json.bak
-jq ".docker.artifacts=\"${BUILD_URL}\"" "${BUILD_JSON}.bak" > "${BUILD_JSON}"
-cp build.json build.json.bak
-jq ".docker.version=\"${DOCKER_VERSION}\"" "${BUILD_JSON}.bak" > "${BUILD_JSON}"
-rm build.json.bak
-jq 'with_entries(select(.key == ("docker")))' build.json > editions.moby/version.json
+jq ".docker.artifacts=\"${BUILD_URL}\"" "$BUILD_JSON" |
+  jq ".docker.version=\"${DOCKER_VERSION}\"" > "$BUILD_JSON".tmp
+mv "$BUILD_JSON.tmp" "$BUILD_JSON"
+
+if [ -d editions.moby ]; then
+    jq 'with_entries(select(.key == ("docker")))' "$BUILD_JSON" > editions.moby/version.json
+fi
 git diff
 git commit -asm "Update docker-ce to ${DOCKER_VERSION} (${DOCKER_COMMIT})"
 
